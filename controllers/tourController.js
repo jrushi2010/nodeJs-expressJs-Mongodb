91a7e137a1d68ab6db57cb0c0e8a526eeb7efcdf
jrushi2010 <jrushi2010@gmail.com>
2023-11-04 17:18:56 +0530

    added param middleware and removed the own created id checker from the functions

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,19 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+
+    console.log(`Tour id is: ${val}`);
+
+    if (req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+    }
+    next();
+}
+
 //for get all tour
 exports.getAllTours = (req, res) => {
 
@@ -31,13 +44,6 @@ exports.getTour = (req, res) => {
     //the find method will then do its that basically it will create an array which only
     //contains the element where this comparision here turns out to be true
     const tour = tours.find(el => el.id === id)
-
-    if (!tour) {
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        });
-    }
 
     res.status(200).json({
         status: 'success',
@@ -72,14 +78,6 @@ exports.createTour = (req, res) => {
 
 //for update
 exports.updateTour = (req, res) => {
-
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(200).json({
         status: 'success',
         data: {
@@ -90,14 +88,6 @@ exports.updateTour = (req, res) => {
 
 //for delete
 exports.deleteTour = (req, res) => {
-
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID'
-        });
-    }
-
     res.status(204).json({
         status: 'success',
         data: null
