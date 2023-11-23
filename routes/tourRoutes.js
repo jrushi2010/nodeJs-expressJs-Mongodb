db56a3f56c9990872c714847e7f7a4e72bb7e4dc
jrushi2010 <jrushi2010@gmail.com>
2023-11-23 10:20:29 +0530

    aggregation pipeline for data statistics

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,6 +11,9 @@ router
     .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
+    .route('/tour-stats').get(tourController.getTourStats);
+
+router
     .route('/')
     .get(tourController.getAllTours)
     //old code
