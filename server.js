e9192c5b989280f5b8c9eddb9272f229f64e2eae
jrushi2010 <jrushi2010@gmail.com>
2024-06-03 12:32:43 +0530

    handled Unhandled Rejections errors

--- server.js
+++ server.js
@@ -23,16 +23,22 @@ mongoose.connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-}).then(con => {
-    // console.log(con.connections);
-    console.log('DB connection successfull');
 })
+    .then(() => console.log('DB connection successfull')).catch(err => console.log('ERROR'));
 
 //console.log(app.get('env')); -> development
 //console.log(process.env);
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`app running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION! Shuting down..');
+    server.close(() => {
+        process.exit(1);
+    })
 });
