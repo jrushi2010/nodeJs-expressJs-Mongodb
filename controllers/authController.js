03754b30d4d77cef5542ab7faef36f513b25ef2c
jrushi2010 <jrushi2010@gmail.com>
2024-06-05 16:48:58 +0530

    password reset functionality: created password reset token and password reset expired timer functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,3 +108,22 @@ exports.restrictTo = (...roles) => {
         next();
     };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+    //1) get user based on posted email
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new AppError('There is no user with email address.', 404));
+    }
+    //2) generate the random reset token
+    const resetToken = user.createPasswordResetToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    //3) send it to users email
+})
+
+exports.resetPassword = (req, res, next) => {
+
+}
