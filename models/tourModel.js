26861bf47a301646fa62e952ed3fc6195cd1536b
jrushi2010 <jrushi2010@gmail.com>
2024-06-20 15:08:41 +0530

    Polpulating tour guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -141,11 +141,22 @@ tourSchema.pre(/^find/, function (next) {
     next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'guides',
+        seletc: '-__v -passwordChangedAt'
+    });
+
+    next();
+})
+
+
 tourSchema.post(/^find/, function (docs, next) {
     console.log(`Query took ${Date.now() - this.start} milliseconds!`);
     console.log(docs);
     next();
 });
+
 
 //aggregation middleware
 tourSchema.pre('aggregate', function (next) {
